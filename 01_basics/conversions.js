9b89bd23d8a68f078d63bd532b6ca33c00b5d880
umarrasheed77 <rasheedumar693@gmail.com>
2025-10-04 15:54:57 +0000

    changed

--- 01_basics/conversions.js
+++ 01_basics/conversions.js
@@ -8,3 +8,14 @@ console.log(typeof(valueInNumber))
 let isLoggedIn=1
 let booleanisLoggedIn= Boolean(isLoggedIn)
 console.log(booleanisLoggedIn)
+
+
+//operations-----------------------huehue
+
+
+ let value=3
+ let negvalue=-value
+ console.log(negvalue)
+ let str1="hitesh"
+ let str2=" bhau"
+ console.log(str1+str2)
