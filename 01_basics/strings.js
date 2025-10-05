1d10165654ab811f981a19ed70cc7df1b9943b50
umarrasheed77 <rasheedumar693@gmail.com>
2025-10-05 13:58:00 +0000

    srtingd

---
+++ 01_basics/strings.js
@@ -0,0 +1,11 @@
+const name="rory"
+const repocount=20
+console.log(name+repocount)
+console.log(`hello mt name is ${name} and myrepo count is ${repocount}`)
+const place=new String('delhi')
+console.log(place.toUpperCase())
+//console.log(place.charAt(e)) aise e nhi likh slte
+  console.log(place.charAt(2))
+
+  const newplace=place.substring(0,2)
+  console.log(newplace)
