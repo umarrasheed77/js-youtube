c73b8df6b0468c755bb1cdab4c5211553b9d493f
umarrasheed77 <rasheedumar693@gmail.com>
2025-10-05 14:21:06 +0000

    numbers

--- 01_basics/numbersandmaths.js
+++ 01_basics/numbersandmaths.js
@@ -1,1 +1,16 @@
 const score=90
+console.log(score)
+const balance= new Number(100)
+console.log(balance)
+
+
+
+console.log(Math)
+console.log(Math.abs(-4))
+console.log(Math.round(4.3))
+console.log(Math.random())
+console.log(Math.random()*10)
+console.log(Math.floor(Math.random()*10))
+const min=10
+const max=20
+
