c001c9a4939ec620f0e5164dadf80c63580c67d5
umarrasheed77 <rasheedumar693@gmail.com>
2025-10-21 09:46:47 +0000

    scope

---
+++ scope.js
@@ -0,0 +1,10 @@
+if(true){
+    let a =10;
+const b=20
+var c=90
+}
+//console.log(a)
+//console.log(b)
+console.log(c)
+// nested scope me chota function upar walwe se le skta hai methods but upar wala mnhi le skta hai
+ 
